9ee1d9700f06c6ed8c9f78914d84a0fa4f5a4dc9
mabai <mabai@10.50.16.139>
2022-03-21 14:53:24 +0000

    Making Entry even tidier

--- eDepot - System/src/platform/Entry.java
+++ eDepot - System/src/platform/Entry.java
@@ -4,8 +4,8 @@ public class Entry {
 
 	// Small static main method to ensure NQF5 best practice
 	public static void main(String[] args) {  
-		Sys system = new Sys();
-		system.run();
+		
+		new Sys().run();
 
 	}
 
