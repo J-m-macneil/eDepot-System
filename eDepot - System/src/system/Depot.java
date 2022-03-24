f444d4e108579f6e8c8bd21534641cfd6e836aa4
mabai <mabai@DESKTOP-RM6KJOM>
2022-03-24 21:49:29 +0000

    Commenting out buggy method

--- eDepot - System/src/system/Depot.java
+++ eDepot - System/src/system/Depot.java
@@ -12,37 +12,39 @@ public class Depot {
 	} 
 
 	
-	public void logOn(String username, String password) {
-		boolean found = false;
-		String tempUsername = "";
-		String tempPassword = "";
-	    try {
-	    	Scanner file = new Scanner(new File("Managers.txt"));
-	    	Scanner input = new Scanner(System.in);
-	    	file.useDelimiter("(,\n)");
-	        
-	        while (input.hasNext() && file.hasNext() && !found) {
-	        	tempUsername = file.next();
-	        	tempPassword = file.next();
-	            if (tempUsername.trim().equals(username.trim()) && tempPassword.trim().equals(password.trim())) {
-	               // Mainemenu menu = new Mainemenu();
-	               //  menu.AdminMenu();
-	               // Above code is a template to let us add different menus but for now see below
-	            	System.out.println("Login Successful!");
-	            	found = true;
-	            	
-	            } else {
-	                System.out.println("--Error--! Press 'Enter' to continue...");
-	                input.nextLine();
-	            }
-	            input.close();
-            	file.close();
-	        }
-	    } catch (IOException e) {
-	        e.getCause();
-	    }
-	    
-	}
+//	public void logOn(String username, String password) {
+//		boolean found = false;
+//		String tempUsername = "";
+//		String tempPassword = "";
+//	    try {
+//	    	Scanner file = new Scanner(new File("Managers.txt"));
+//	    	Scanner input = new Scanner(System.in);
+//	    	file.useDelimiter("(,\n)");
+//	        
+//	        while (input.hasNext() && file.hasNext() && !found) {
+//	        	tempUsername = file.next();
+//	        	tempPassword = file.next();
+//	            if (tempUsername.trim().equals(username.trim()) && tempPassword.trim().equals(password.trim())) {
+//	               // Mainemenu menu = new Mainemenu();
+//	               //  menu.AdminMenu();
+//	               // Above code is a template to let us add different menus but for now see below
+//	            	System.out.println("Login Successful!");
+//	            	found = true;
+//	            	
+//	            } else {
+//	                System.out.println("--Error--! Press 'Enter' to continue...");
+//	                input.nextLine();
+//	            }
+//	            input.close();
+//            	file.close();
+//	        }
+//	    } catch (IOException e) {
+//	        e.getCause();
+//	    }
+//	    
+//	}
+	
+	
 
 	public void setUpWorkSchedule() { // add method body
 		
