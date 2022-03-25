560295d69237f4403eb9aa1999f5b9593147505c
mabai <mabai@10.50.16.94>
2022-03-25 14:46:20 +0000

    Minor changes

--- eDepot - System/src/system/Depot.java
+++ eDepot - System/src/system/Depot.java
@@ -9,20 +9,20 @@ public class Depot {
 
 	final static Scanner input = new Scanner(System.in);
 
-	final static ArrayList<Manager> manager = new ArrayList<Manager>();
+	final static ArrayList<Driver> drivers = new ArrayList<Driver>();
 
 	// public Depot() {
 	// }
 
 	// Declare a 'loadDriver' method, to allow the user data to be access through
 	// the method.
-	public void loadManager() {
+	public void loadDriver() {
 
 		// Declaring a Scanner with 'driver.txt' source, to allow the system to read the
 		// user data.
 		Scanner file = null;
 		try {
-			file = new Scanner(new FileReader("src//Managers.txt"));
+			file = new Scanner(new FileReader("src//Drivers.txt"));
 			// Loop through the files data.
 			while (file.hasNext()) {
 				// Set the username to the next String in the file.
@@ -30,7 +30,7 @@ public class Depot {
 				// Set the password to the next String in the file.
 				String password = file.nextLine();
 				// Add the variables to the 'User' array list.
-				manager.add(new Manager(username, password));
+				drivers.add(new Manager(username, password));
 				// Close the scanner file.
 			}
 		} catch (FileNotFoundException e) {
@@ -41,15 +41,15 @@ public class Depot {
 
 	// Declare a 'logOn' method.
 	public void logOn(String userName, String passWord) {
-		loadManager();
+		loadDriver();
 		boolean correctUsername = false;
 		boolean correctPassword = false;
 		boolean loggedOn = false;
-		Manager logOn = null;
+		Driver logOn = null;
 		// Loop through the User file data.
-		for (int i = 0; i < manager.size(); i++) {
+		for (int i = 0; i < drivers.size(); i++) {
 			// Get the User file data.
-			logOn = manager.get(i);
+			logOn = drivers.get(i);
 			// Declare an if statement to match the username and password, against the
 			// Driver file data.
 			if (logOn.checkUsername(userName)) {
@@ -81,8 +81,8 @@ public class Depot {
 			loggedOn = false;
 			System.out.print("\nYour username and password is invalid");
 		}
-		if (manager.size() == 0) {
-			System.out.println("No managers enrolled in depot!");
+		if (drivers.size() == 0) {
+			System.out.println("Everyone's on the roads!");
 		}
 
 	}
