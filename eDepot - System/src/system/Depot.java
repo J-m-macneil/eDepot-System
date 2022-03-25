cfd1a1664604a94e86bd54f3084fc07c4ca5876f
mabai <mabai@10.50.16.94>
2022-03-25 13:42:57 +0000

    Deleted commented out method

--- eDepot - System/src/system/Depot.java
+++ eDepot - System/src/system/Depot.java
@@ -15,39 +15,6 @@ public class Depot {
 	public Depot() {
 	}
 	
-	
-//	public void logOn(String username, String password) {
-//		boolean found = false;
-//		String tempUsername = "";
-//		String tempPassword = "";
-//	    try {
-//	    	Scanner file = new Scanner(new File("Managers.txt"));
-//	    	Scanner input = new Scanner(System.in);
-//	    	file.useDelimiter("(,\n)");
-//	        
-//	        while (input.hasNext() && file.hasNext() && !found) {
-//	        	tempUsername = file.next();
-//	        	tempPassword = file.next();
-//	            if (tempUsername.trim().equals(username.trim()) && tempPassword.trim().equals(password.trim())) {
-//	               // Mainemenu menu = new Mainemenu();
-//	               //  menu.AdminMenu();
-//	               // Above code is a template to let us add different menus but for now see below
-//	            	System.out.println("Login Successful!");
-//	            	found = true;
-//	            	
-//	            } else {
-//	                System.out.println("--Error--! Press 'Enter' to continue...");
-//	                input.nextLine();
-//	            }
-//	            input.close();
-//            	file.close();
-//	        }
-//	    } catch (IOException e) {
-//	        e.getCause();
-//	    }
-//	    
-//	}
-	
 	// Declare a 'loadDriver' method, to allow the user data to be access through the method.
 			public void loadManager() throws FileNotFoundException {
 				
