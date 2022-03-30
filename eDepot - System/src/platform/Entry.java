fbe22057e32504c6be919167b863494429dcf095
imClarkey <liamcclarke12@gmail.com>
2022-03-30 21:55:58 +0100

    lets hope this worked

--- eDepot - System/src/platform/Entry.java
+++ eDepot - System/src/platform/Entry.java
@@ -6,7 +6,6 @@ public class Entry {
 
 	// Small static main method to ensure NQF5 best practice
 	public static void main(String[] args) throws FileNotFoundException {  
-		
 		new Sys().entryMenu();
 
 	}
