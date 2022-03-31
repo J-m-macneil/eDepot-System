25e35b21e41619660b1ba4a64d9329dc57b5fe49
mabai <mabai@10.50.16.114>
2022-03-31 16:52:18 +0100

    Schedulabale interface made

--- eDepot - System/src/system/Schedulable.java
+++ eDepot - System/src/system/Schedulable.java
@@ -1,5 +1,11 @@
 package system;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface Schedulable {
 
+	public boolean isAvailable(LocalDateTime startDate, LocalDateTime endDate);
+	public void addSchedule(WorkSchedule schedule);
+	public List<WorkSchedule>getSchedule();
 }
