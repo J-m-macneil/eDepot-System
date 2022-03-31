5ade6d18fa15a19fb2d4b399653e86392fe86e35
mabai <mabai@10.50.16.114>
2022-03-31 17:04:37 +0100

    Added Glyn's methods to Driver and it implements Schedulable

--- eDepot - System/src/system/Driver.java
+++ eDepot - System/src/system/Driver.java
@@ -2,12 +2,18 @@ package system;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Driver {
+public class Driver implements Schedulable {
 
 	protected String username;
 	protected String password;
+	protected Boolean update = true;
+	
+	protected List<WorkSchedule> schedule = new LinkedList<WorkSchedule>();
 
 	public Driver(String username, String passWord) {
 
@@ -74,5 +80,33 @@ public class Driver {
 			e.printStackTrace();
 		}
 		return loggedOn;
+	}
+
+	@Override
+	public boolean isAvailable(LocalDateTime startDate, LocalDateTime endDate) {
+		for(WorkSchedule s : schedule) {
+			if((s.getStartDate().isBefore(startDate)) && (s.getEndDate().isAfter(startDate))) {
+				return false;
+			}
+			if((s.getStartDate().isBefore(endDate)) && (s.getEndDate().isAfter(endDate))) {
+				return false;
+			}
+			if((s.getStartDate().isAfter(startDate)) && (s.getEndDate().isBefore(endDate))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public void addSchedule(WorkSchedule schedule) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<WorkSchedule> getSchedule() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
