fa0e4fbe80350929676b1db8cf2b346bb0d46da6
Joseph Macneil <j.m.macneil@hotmail.com>
2022-04-01 14:39:16 +0100

    Sys.

--- eDepot - System/src/platform/Sys.java
+++ eDepot - System/src/platform/Sys.java
@@ -1,6 +1,7 @@
 package platform;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 // Used to serialize data
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -14,34 +15,38 @@ import java.util.List;
 // import java.time.LocalDate;
 import java.util.Scanner;
 import system.Depot;
+import system.Driver;
 import system.Status;
 import system.WorkSchedule;
+
 
 public class Sys {
 
 	// This path is exclusive to me Liam and is used for testing
-	private final String PATH = "E:\\University\\Work\\Year 2\\Semester 2\\5104 - Object Orientated Systems\\Graded\\Assignments\\Assignment 2\\Serialized data\\";
-	private String userName;
-	private String passWord;
+	//private final String PATH = "E:\\University\\Work\\Year 2\\Semester 2\\5104 - Object Orientated Systems\\Graded\\Assignments\\Assignment 2\\Serialized data\\";
+	// This path is exclusive to Joe and is used for testing.
+	private final String PATH = "D:\\OOP Coursework\\";
+	
 	private List<Depot> depots = new ArrayList<Depot>();
 	private Depot depot;
-
+	private String currentUser;
+	
 	private static final Scanner input = new Scanner(System.in);
 
 	public Sys() {
 		deSerialize();
 
-		// depots.add(new Depot("Liverpool"));
-		// depots.add(new Depot("Manchester"));
-		// depots.add(new Depot("Leeds"));
+		depots.add(new Depot("Liverpool"));
+		depots.add(new Depot("Manchester"));
+		depots.add(new Depot("Leeds"));
 	}
 
-	public void entryMenu() {
+	public void entryMenu() throws Exception {
 
 		String choice = "";
 		do {
-			System.out.println("\n-- ENTRY MENU --\n");
-			System.out.println("1 - Login");
+			System.out.println(" -- Entry Menu -- ");
+			System.out.println("1 - LogOn");
 			System.out.println("Q - Quit");
 			System.out.print("Pick: ");
 			choice = input.nextLine();
@@ -49,63 +54,56 @@ public class Sys {
 			case "1":
 				logOn();
 				break;
-			case "Q":
-				System.out.println("-- GOODBYE --");
-				System.exit(0);
-			default:
-				System.out.println("Invalid input!");
-				break;
 			}
-		} while (!choice.equals("Q"));
-
+		} while (!choice.toUpperCase().equals("Q"));
 		serialize();
-
-		System.exit(0);
 	}
-
-	private void logOn() {
-		Depot depot = new Depot();
-		// ? System.out.println("Liverpool\nManchester\nLeeds");
-		System.out.println("\n-- LOGIN --\n");
-		System.out.print("Location : ");
+	
+	public void logOn() throws Exception {	
+		System.out.println("\nLiverpool\nManchester\nLeeds\n");
+		
+		System.out.print("Location: ");
 		String location = input.nextLine();
+		
 		System.out.print("Username : ");
-		userName = input.nextLine();
+		String username = input.nextLine();
+		
 		System.out.print("Password : ");
-		passWord = input.nextLine();
-		// if (depot.logOn(userName, passWord)) {
-
-		// depot = getDepot(location);
-		if (depot.logOn(userName, passWord)) {
-			System.out.println("Correct!");
+		String password = input.nextLine();
+		
+		depot = getDepotLocation(location);
+		if (depot.logOn(username, password)) {
+			currentUser = username;
+			System.out.println("Correct! Logged on as user: " + currentUser);
 			driverMenu();
-		} else if (depot.logOnAsManager(userName, passWord)) {
-			System.out.println("Correct!");
+		} else if (depot.logOnAsManager(username, password)) {
+			currentUser = username;
+			System.out.println("Correct! Logged on as manager: "+ currentUser);
 			managerMenu();
 		} else {
 			System.out.println("Invalid login!");
 			entryMenu();
 		}
 	}
-	// }
 
-	/*
-	 * public Depot getDepot(String location) { for (Depot d : depots) { if
-	 * (location.equals(d)) { return d; } } return depot;
-	 * 
-	 * }
-	 */
+	public Depot getDepotLocation(String location) {
+		for (Depot d : depots) {
+			if (location.equals(d.getLocation())) {
+				return d;
+			}
+		}
+		return null;
+	}
 
-	private void driverMenu() {
+	public void driverMenu() {
 		// Declare a do while loop, to repeat through the depot systems main menu.
 		// Set a default value to choice, to allow user input.
 		String choice = "";
 		do {
-			// String userName = null;
 			// Print a repeating main menu, inside the do while loop.
-			System.out.println("\n-- " + userName + "'s MAIN MENU --\n");
+			System.out.println("\n-- " + currentUser + "'s MAIN MENU --");
 			System.out.println("1 - View Work Schedule");
-			System.out.println("Q - Quit");
+			System.out.println("2 - Log Off");
 			System.out.print("Pick : ");
 
 			// Allow the user to specify which option on they would like to select.
@@ -120,9 +118,7 @@ public class Sys {
 				// displaySchedule
 				break;
 			}
-			case "Q": {
-				System.out.println("-- GOODBYE --");
-				System.exit(0);
+			case "2": {
 				break;
 			}
 			// Set a default value, for when errors occur in the console application.
@@ -133,21 +129,21 @@ public class Sys {
 			}
 			}
 			// Declare a while loop, to loop through the menu until the program is quit.
-		} while (!choice.equals("Q"));
+		} while (!choice.equals("2"));
 
 	}
 
-	private void managerMenu() {
+	public void managerMenu() throws Exception {
 		// Declare a do while loop, to repeat through the depot systems main menu.
 		// Set a default value to choice, to allow user input.
 		String choice = "";
 		do {
 			// Print a repeating main menu, inside the do while loop.
-			System.out.println("\n-- " + userName + "'s MAIN MENU --\n");
+			System.out.println("\n-- " + currentUser + "'s MAIN MENU --");
 			System.out.println("1 - View Work Schedule");
 			System.out.println("2 - Create Work Schedule");
 			System.out.println("3 - Re-assign Vehicle");
-			System.out.println("Q - Quit");
+			System.out.println("4 - Log Off");
 			System.out.print("Pick : ");
 
 			// Allow the user to specify which option on they would like to select.
@@ -158,35 +154,33 @@ public class Sys {
 			// executed.
 			switch (choice) {
 			case "1": {
-				// Call displaySchedule() method.
-				displaySchedule();
+				// Set a choice for the method 'createSchedule' to be executed.
+				// displaySchedule
 				break;
 			}
 			case "2": {
-				// Call createSchedule() method.
+				// Set a choice for the method 'createSchedule' to be executed.
 				createSchedule();
 				break;
 			}
 			case "3": {
-				// Call reassignVehicle() method.
+				// Set a choice for the method 'reassignVehicle' to be executed.
 				reassignVehicle();
 				break;
 			}
-			case "Q": {
+			case "4": {
 				// Print a message to the console when the application is quit.
-				System.out.println("-- GOODBYE --");
-				System.exit(0);
 				break;
 			}
 			// Set a default value, for when errors occur in the console application.
 			default: {
 				// Set a default message, to allow the user to know when an incorrect value has
 				// been entered.
-				System.out.println("I'm sorry you have entered an incorrect value, please try again");
+				System.out.println("Im sorry you have entered an incorrect value, please try again:");
 			}
 			}
 			// Declare a while loop, to loop through the menu until the program is quit.
-		} while (!choice.equals("Q"));
+		} while (!choice.equals("4"));
 
 	}
 
@@ -216,45 +210,35 @@ public class Sys {
 			System.out.println(e.getMessage());
 		}
 	}
-
+	
 	private void displaySchedule() {
-		// Make case 1 in here
-
+		
 	}
 
-	private void createSchedule() {
-		System.out.println("Clients name: ");
-		String client = input.nextLine();
+	private void createSchedule() throws Exception {
+		System.out.println("\n-- CREATE SCHEDULE --\n");
+		
+		System.out.print("Clients name: ");
+		String client = input.next();
 
-		System.out.println("Schedules DateTime [i.e 5 Oct 18 09:30] : ");
-		LocalDateTime startDate = LocalDateTime.parse(input.nextLine(), DateTimeFormatter.ofPattern("d MMM yy HH:mm"));
+		System.out.print("Schedules DateTime [i.e 5 Oct 18 09:30]: ");
+		LocalDateTime startDate = LocalDateTime.parse(input.next(), DateTimeFormatter.ofPattern("d MMM yy HH:mm"));
 
-		System.out.println("Schedules DateTime [i.e 5 Oct 18 09:30] : ");
-		LocalDateTime endDate = LocalDateTime.parse(input.nextLine(), DateTimeFormatter.ofPattern("d MMM yy HH:mm"));
+		System.out.print("Schedules DateTime [i.e 5 Oct 18 09:30]: ");
+		LocalDateTime endDate = LocalDateTime.parse(input.next(), DateTimeFormatter.ofPattern("d MMM yy HH:mm"));
 
-		depot.createSchedule(new WorkSchedule(client, startDate, endDate, Status.PENDING));
+		System.out.print("Drivers name: ");
+		String driver = input.next();
+		
+		System.out.print("Vehicle Registration number: ");
+		String vehicle = input.next();
+		
+		depot.createSchedule(new WorkSchedule(client, startDate, endDate, driver, vehicle));
 	}
 
 	private void reassignVehicle() {
-		while (true) {
-			System.out.println("\n-- RE-ASSIGN VEHICLE MENU --\n");
-			System.out.println("\n-- Please specify what type of Vehicle you are moving --\n");
-			System.out.print("Pick (Either 'Truck' or 'Tanker'): ");
-			String type = input.next();
-			if (type.equals("Truck")) {
-				System.out.print("Enter Truck's make : ");
-				String make = input.next();
-				depot.getVehicleByMake((make));
-				// Not working - check getVehicleByMake() in Depot class
-
-			} else if (type.equals("Tanker")) {
-
-			} else {
-				System.out.println("Invalid vehicle type. Try again");
-				continue; // Manager is not kicked out to main menu if they make a mistake
-			}
-		}
-
+		System.out.println("--\nRE-ASSIGN VEHICLE MENU--\n");
+		System.out.print("");
 	}
 
 }
