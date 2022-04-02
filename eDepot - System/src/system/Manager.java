f8be3fca69a3b70132c0be46a9d4e1ef644d70d2
mabai <mabai@DESKTOP-RM6KJOM.Home>
2022-04-02 21:34:53 +0100

    Added serial version id to Manager class as it complained

--- eDepot - System/src/system/Manager.java
+++ eDepot - System/src/system/Manager.java
@@ -2,6 +2,11 @@ package system;
 
 public class Manager extends Driver {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public Manager(String userName, String passWord) {
 		super(userName, passWord);
 	}
